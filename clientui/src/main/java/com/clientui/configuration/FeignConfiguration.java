bceb1b57e55fd0e2f1241bdbe8a2a04ecbb97231
Hoareau John <hxjwork1@gmail.com>
2021-01-16 21:29:58 +0400

    Finished chapter 9

---
+++ clientui/src/main/java/com/clientui/configuration/FeignConfiguration.java
@@ -0,0 +1,14 @@
+package com.clientui.configuration;
+
+import feign.auth.BasicAuthRequestInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfiguration {
+
+    @Bean
+    public BasicAuthRequestInterceptor basicAuthRequestInterceptor() {
+        return new BasicAuthRequestInterceptor("utilisateur", "mdp");
+    }
+}
