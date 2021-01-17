2fd6ed83de561005b2562996a7d926e16d099ef3
Hoareau John <hxjwork1@gmail.com>
2021-01-17 19:20:33 +0400

    Finished course

--- spring-admin/src/main/java/com/mcommerce/springadmin/SpringAdminApplication.java
+++ spring-admin/src/main/java/com/mcommerce/springadmin/SpringAdminApplication.java
@@ -1,16 +1,16 @@
-package com.mcommerce.configserver;
+package com.mcommerce.springadmin;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer
+@EnableAdminServer
 @EnableDiscoveryClient
-public class ConfigServerApplication {
+public class SpringAdminApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
+		SpringApplication.run(SpringAdminApplication.class, args);
 	}
 }
